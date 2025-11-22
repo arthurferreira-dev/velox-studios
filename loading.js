1099bb78bcd2c525887c0813044b4b953a2277dc
Arthur Ferreira <tutudacosta996@gmail.com>
2025-11-22 13:22:38 -0300

    Update loading.js

--- loading.js
+++ loading.js
@@ -1,10 +1,10 @@
 document.addEventListener('DOMContentLoaded', () => {
-    let img = document.querySelector('img#loading-anim');
-    img.src = 'network/loading-update.gif'
+    let img = document.querySelector('img#loading-done');
+    img.src = 'network/loading-done.gif'
     img.setAttribute('width', '100')
-    img.setAttribute('alt', 'Animated GIF Loading Update');
+    img.setAttribute('alt', 'Animated GIF Loading Done');
 });
 
 document.querySelector('button').addEventListener('click', () => {
-    window.location = 'https://discord.com/invite/YZy82288'
+    window.location = 'https://velox-studios.vercel.app'
 });
